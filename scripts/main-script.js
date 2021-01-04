72e053c8a17b1ec6257a8fcd14f96184dcff3163
linishere <miyamoto.linaly@gmail.com>
2021-01-03 19:32:16 -0800

    yes

---
+++ scripts/main-script.js
@@ -0,0 +1,9 @@
+(function(a){a.expr[":"].onScreen=function(b){var c=a(window),d=c.scrollTop(),e=c.height(),f=d+e,g=a(b),h=g.offset().top,i=g.height(),j=h+i;return h>=d&&h<f||j>d&&j<=f||i>e&&h<=d&&j>=f}})(jQuery);
+
+$(function() {
+  setInterval(function() {
+    $("section")
+      .filter(":onScreen")
+        .addClass('is-visible')
+  }, 500)
+})
